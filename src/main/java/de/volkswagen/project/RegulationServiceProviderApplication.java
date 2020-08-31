b9d6b639488b6a48a82008fb3ee799ebbaaba4c7
Dennis <d.daumlechner@gmail.com>
2020-08-31 13:48:29 +0200

    initialMavenProject

---
+++ src/main/java/de/volkswagen/project/RegulationServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegulationServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegulationServiceProviderApplication.class, args);
+	}
+
+}
